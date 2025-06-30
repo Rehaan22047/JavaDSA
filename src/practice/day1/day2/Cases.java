fe100c69199befa059d065c37fd028b7121f0368
Rehaan22047 <rehaans2047@gmail.com>
2025-06-30 16:00:42 +0530

    day 2

--- src/practice/day1/day2/Cases.java
+++ src/practice/day1/day2/Cases.java
@@ -6,36 +6,35 @@ public class Cases {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Hello and Welcome!");
-        System.out.print("enter the first number:");
-        float num1 = input.nextFloat();
+        System.out.print("enter the first number: ");
+        int num1 = input.nextInt();
         System.out.print("enter the second number: ");
-        float num2 = input.nextInt();
-        System.out.println("choose an operation: ");
-        System.out.println("1: + Addition");
-        System.out.println("2: - subtraction");
-        System.out.println("3: * Multiplication");
-        System.out.println("4: / Division");
-        System.out.println("5: % Modulus");
+        int num2 = input.nextInt();
+        System.out.println("select an operation: ");
+        System.out.println("1. + addition");
+        System.out.println("2. - subtraction");
+        System.out.println("3. * multiplication");
+        System.out.println("4. / Division");
+        System.out.println("5. % modulo");
         int choice = input.nextInt();
-
-        switch (choice) {
+        switch (choice){
             case 1:
-                System.out.println("Result: " + (num1 + num2));
+                System.out.println("The result is: "+(num1+num2));
                 break;
             case 2:
-                System.out.println("Result: " + (num1 - num2));
+                System.out.println("The result is: "+(num1-num2));
                 break;
             case 3:
-                System.out.println("Result: " + (num1 * num2));
+                System.out.println("The result is: "+(num1*num2));
                 break;
             case 4:
-                if (num2 != 0)
-                    System.out.println("Result: " + (num1 / num2));
-                else
-                    System.out.println("Infinite");
+                if (num2 != 0){
+                    System.out.println("The result is: "+(num1/num2));
+                }else
+                    System.out.println("Infinite!!!!");
                 break;
             default:
-                System.out.println("Result: " + (num1 % num2));
+                System.out.println("The result is: "+num1%num2);
         }
     }
 }
